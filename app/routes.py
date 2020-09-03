96df60de0b00b5999335aa3723940846398b9841
geihar <l0635968488@gmail.com>
2020-09-03 12:19:12 +0200

    add login/logout

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, redirect, flash, url_for
+from flask import Flask, render_template, redirect, flash, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from app import app
+from app.models import User
 from .forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Dima'}
     posts = [
@@ -27,8 +31,22 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+        if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from app import app, db
-from app.models import User
+from app import app, db, login
+from app.models import User, Post
 
 
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {'db': db, 'User': User, }
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
