40c502a623d45f97bf8530e56d54cb42ce09bfb2
geihar <l0635968488@gmail.com>
2020-09-22 13:18:55 +0200

    add post pagination

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config(object):
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+    POSTS_PER_PAGE = 3
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
